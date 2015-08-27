4a11428a7750a6a2ddee6411e3bf7304765c92b5
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2015-08-27 11:56:20 -0700

    Initial commit
    Create Menu, Food Store, and Items

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('menu');
+  this.route('item');
+  this.route('food-store');
 });
 
 export default Router;
---
+++ tests/unit/routes/food-store-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:food-store', 'Unit | Route | food store', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
